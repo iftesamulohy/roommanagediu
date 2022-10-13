7efed1084d5958f95396f45a655c6582bd65bf51
iftesamulohy <64037194+iftesamulohy@users.noreply.github.com>
2022-10-14 05:07:30 +0600

    ready for heroku

--- roommanagement/settings.py
+++ roommanagement/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)_3zg4!_(-=j0phk8)vawkby&3qooylvv0%7f7e9t$aqb(sj=j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
